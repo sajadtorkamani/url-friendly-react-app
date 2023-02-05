230a98745bc27f4ff8168ce5ba62a64516372138
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-02-05 12:45:29 +0000

    WIP

--- src/components/SearchFilters.tsx
+++ src/components/SearchFilters.tsx
@@ -1,36 +1,38 @@
 import React from 'react'
 
-const SearchFilters: React.FC = () => (
-  <div>
-    <div className="mb-4">
-      <label htmlFor="job_title" className="mb-1 block">
-        Job title
-      </label>
-      <input
-        type="text"
-        id="job_title"
-        name="job_title"
-        placeholder="Search by job title"
-        className="border border-gray-500 py-1 px-2"
-      />
-    </div>
-
+const SearchFilters: React.FC = () => {
+  return (
     <div>
-      <label htmlFor="job_type" className="mb-1 block">
-        Job type
-      </label>
+      <div className="mb-4">
+        <label htmlFor="title" className="mb-1 block">
+          Job title
+        </label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          placeholder="Search by job title"
+          className="border border-gray-500 py-1 px-2"
+        />
+      </div>
 
-      <select
-        name="type"
-        id="job_type"
-        className="border border-gray-500 px-2 py-1"
-      >
-        <option value="any">Any</option>
-        <option value="permanent">Permanent</option>
-        <option value="contract">Contract</option>
-      </select>
+      <div>
+        <label htmlFor="type" className="mb-1 block">
+          Job type
+        </label>
+
+        <select
+          name="type"
+          id="type"
+          className="border border-gray-500 px-2 py-1"
+        >
+          <option value="any">Any</option>
+          <option value="permanent">Permanent</option>
+          <option value="contract">Contract</option>
+        </select>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default SearchFilters
