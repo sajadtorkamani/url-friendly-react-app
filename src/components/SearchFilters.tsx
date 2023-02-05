8777201ad8c126bd0dca4ce097563daad084e70d
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-02-05 18:06:27 +0000

    WIP

--- src/components/SearchFilters.tsx
+++ src/components/SearchFilters.tsx
@@ -4,6 +4,7 @@ import { useSearchParams } from 'react-router-dom'
 interface State {
   title: string
   type: string
+  location: string[]
 }
 
 type Action =
@@ -11,7 +12,7 @@ type Action =
       type: 'updateFilter'
       payload: {
         name: string
-        value: string
+        value: string | string[]
       }
     }
   | { type: 'clearFilters' }
@@ -24,7 +25,7 @@ function reducer(state: State, action: Action) {
       const { name, value } = action.payload
       return { ...state, [name]: value }
     case 'clearFilters':
-      return { title: '', type: '' }
+      return { title: '', type: '', location: [] }
     default:
       return state
   }
@@ -36,10 +37,10 @@ const SearchFilters: React.FC = () => {
   const INITIAL_STATE: State = {
     title: searchParams.get('title') || '',
     type: searchParams.get('type') || '',
+    location: searchParams.getAll('location') || [],
   }
 
   const [state, dispatch] = useReducer(reducer, INITIAL_STATE)
-
   const hasFilters = Object.values(state).some(Boolean)
 
   useEffect(
@@ -47,11 +48,23 @@ const SearchFilters: React.FC = () => {
       const searchParams = new URLSearchParams()
 
       Object.entries(state).forEach(([key, value]) => {
+        console.log(value)
+
+        // Handle empty values
         if (value === '') {
           searchParams.delete(key)
-        } else {
-          searchParams.set(key, value)
+          return
         }
+
+        // Handle multiple values
+        if (Array.isArray(value)) {
+          searchParams.delete(key) // Delete all values and rebuild in next line
+          value.forEach((arrayValue) => searchParams.append(key, arrayValue))
+          return
+        }
+
+        // Handle scalar values
+        searchParams.set(key, value)
       })
 
       setSearchParams(searchParams)
@@ -59,7 +72,7 @@ const SearchFilters: React.FC = () => {
     [state]
   )
 
-  function handleFilterChange(name: Filter, value: string) {
+  function handleFilterChange(name: Filter, value: string | string[]) {
     dispatch({
       type: 'updateFilter',
       payload: { name, value },
@@ -86,7 +99,7 @@ const SearchFilters: React.FC = () => {
         />
       </div>
 
-      <div>
+      <div className="mb-4">
         <label htmlFor="type" className="mb-1 block">
           Job type
         </label>
@@ -103,6 +116,32 @@ const SearchFilters: React.FC = () => {
           <option value="">Any</option>
           <option value="permanent">Permanent</option>
           <option value="contract">Contract</option>
+        </select>
+      </div>
+
+      <div>
+        <label htmlFor="location" className="mb-1 block">
+          Location
+        </label>
+
+        <select
+          name="location"
+          multiple
+          className="border border-gray-500 px-2 py-1"
+          value={state.location}
+          id="location"
+          onChange={(event) => {
+            const selectedValues = [...event.target.options]
+              .filter((option) => option.selected)
+              .map((option) => option.value)
+
+            handleFilterChange('location', selectedValues)
+          }}
+        >
+          <option value="london">London</option>
+          <option value="manchester">Manchester</option>
+          <option value="birmingham">Birmingham</option>
+          <option value="leeds">Leeds</option>
         </select>
       </div>
 
