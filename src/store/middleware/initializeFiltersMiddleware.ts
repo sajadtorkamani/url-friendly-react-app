ad1af8db49903623d0a8dadf4c00ed292b331a3b
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-09-24 18:02:44 +0100

    WIP

--- src/store/middleware/initializeFiltersMiddleware.ts
+++ src/store/middleware/initializeFiltersMiddleware.ts
@@ -1,8 +1,23 @@
 import { Middleware } from '@reduxjs/toolkit'
+import { updateFilters } from '../slices/searchSlice'
 
 const initFromUrlMiddleware: Middleware = (storeAPI) => (next) => (action) => {
   if (action.type === 'filters/initFromUrl') {
-    console.log(storeAPI)
+    switch (window.location.pathname) {
+      case '/': {
+        const queryParams = new URLSearchParams(window.location.search)
+
+        queryParams.forEach((value, key) => {
+          storeAPI.dispatch(updateFilters({ [key]: value }))
+          console.log({ key, value })
+        })
+
+        break
+      }
+      default:
+        // Do nothing
+        break
+    }
   }
   next(action)
 }
